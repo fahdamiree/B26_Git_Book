ad68b2ff8c09ea238b9edd95de88c31f16498d77
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-07 03:12:05 -0700

    created love by amiree

---
+++ src/chapter_0/AmireeAtash.java
@@ -0,0 +1,12 @@
+package chapter_0;
+
+public class AmireeAtash {
+    public static void main(String[] args) {
+
+
+        System.out.println("love is nothing");
+
+
+        System.out.println("love is live");
+    }
+}
