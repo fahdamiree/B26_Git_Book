103c65cd06b8c0f302e61b7f9eabf4475d256e52
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-07 03:08:43 -0700

    created by amiree

---
+++ src/chapter_0/AtashAmiree.java
@@ -0,0 +1,19 @@
+package chapter_0;
+
+public class AtashAmiree {
+    public static void main(String[] args) {
+
+        System.out.println("malek amiree and amir amiree");
+
+        // love is live
+
+
+        // love is sold
+
+        // love is sex
+
+
+
+
+    }
+}
